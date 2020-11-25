7bd910c5648a762a76578e608544481a10d78531
Kaiwen <51724164+kaiwen-kakuiii@users.noreply.github.com>
2020-11-26 01:12:09 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+CONFIG = {
+    'gal_type': 'exp',
+    'psf_type': 'gauss',
+    'shear_scene': True,
+    'g1':0.02,
+    'g2':0,
+    'n_coadd': 30,
+    'scale': 0.263,
+    'gal_kws': {'survey_name': 'DES'},
+    'mask_and_interp': True,
+}
